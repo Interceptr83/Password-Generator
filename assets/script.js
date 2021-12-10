8bf9a6fb34afa2d1b83bdf282ffc889f39aaf8f4
Dustin Smith <dustinsmith1983@gmail.com>
2021-12-09 16:30:52 -0800

    created variable to store lists of possible characters. Also created 5 prompts. number of chars, then yes or no for special characters, numbers, lower case, and upper case

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,36 @@
+// Assigning character arrays
+
+// lowercase
+const lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// uppercase
+const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+// numbers
+const numbers = ["1","2","3","4","5","6","7","8","9","0"];
+// special characters
+const specialChar = ["!","'","#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","?","@","[","]","^","_","`","{","|","}","~"]
+
+
+function generatePassword() {
+
+    var pwLength = parseInt(prompt("Please enter the desired length of the password:"), 10);
+    
+    var incSpecialChar = confirm("Would you like to include Special Charcters in your new password?");
+
+    var incNum = confirm("Would you like to inlcude Numbers in your new password?");
+
+    var incLowerCase = confirm("Would you like to include Lower case characters in your new password?");
+
+    var incUpperCase = confirm("Would you like to include Upper case characters in your new password?");
+
+    console.log(pwLength);
+    console.log(incSpecialChar);
+    console.log(incNum);
+    console.log(incLowerCase);
+    console.log(incUpperCase);
+    return null;
+}
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
