a6bdcc85278f735dc5246d88288050105b5745bc
Dustin Smith <dustinsmith1983@gmail.com>
2021-12-10 23:09:28 -0800

    added check to make sure at least one type of character is selected. also added function to create the array of available characters based on what the user selects

--- assets/script.js
+++ assets/script.js
@@ -8,25 +8,61 @@ const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","
 const numbers = ["1","2","3","4","5","6","7","8","9","0"];
 // special characters
 const specialChar = ["!","'","#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","?","@","[","]","^","_","`","{","|","}","~"]
-
+// create variable to store the combined possible characters to be used
+var passOptions = [];
 
 function generatePassword() {
 
     var pwLength = parseInt(prompt("Please enter the desired length of the password:"), 10);
     
-    var incSpecialChar = confirm("Would you like to include Special Charcters in your new password?");
+    var incLowerCase = confirm("Would you like to include Lower case characters in your new password? (OK = Yes, Cancel = No)");
 
-    var incNum = confirm("Would you like to inlcude Numbers in your new password?");
+    var incUpperCase = confirm("Would you like to include Upper case characters in your new password? (OK = Yes, Cancel = No)");
+    
+    var incNum = confirm("Would you like to inlcude Numbers in your new password? (OK = Yes, Cancel = No)");
+    
+    var incSpecialChar = confirm("Would you like to include Special Charcters in your new password? (OK = Yes, Cancel = No)");
 
-    var incLowerCase = confirm("Would you like to include Lower case characters in your new password?");
+// check to make sure they selected at least one
+    if (
+      incLowerCase === false &&
+      incUpperCase === false &&
+      incNum === false &&
+      incSpecialChar === false
+    ) {
+     // alert('Must select at least one character type');
+      return 'WARNING! You must select at least one character type! Please try again.';
+    }
 
-    var incUpperCase = confirm("Would you like to include Upper case characters in your new password?");
+    createCharList();
+
+    function createCharList() {
+
+      if (incSpecialChar === true) {
+        passOptions = passOptions.concat(specialChar);
+      }
+
+      if (incNum === true) {
+        passOptions = passOptions.concat(numbers);
+      }
+
+      if (incLowerCase === true) {
+        passOptions = passOptions.concat(lowerCase);
+      }
+
+      if (incUpperCase === true) {
+        passOptions = passOptions.concat(upperCase);
+      }
+      
+      return passOptions;
+    } 
 
     console.log(pwLength);
     console.log(incSpecialChar);
     console.log(incNum);
     console.log(incLowerCase);
     console.log(incUpperCase);
+    console.log(passOptions);
     return null;
 }
 
